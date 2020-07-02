541d405d73e8169ea62b0d967e130f9b014f1ba2
oradzhabov <oradzhabov@gmail.com>
2020-07-02 13:10:45 +0300

    prep for module external using

--- test_prediction.py
+++ test_prediction.py
@@ -1,76 +1,24 @@
 import os
 import numpy as np
 import cv2
-from kutils import utilites, PrepareData
-from kmodel import data
-from config import cfg
-from kmodel.train import read_sample
-from kmodel.smooth_tiled_predictions import predict_img_with_smooth_windowing
+from kutils import utilites
+from config_rocks import cfg
 from kutils.VIAConverter import *
-
-
-def find_nearest(array, value):
-    array = np.asarray(array)
-    idx = (np.abs(array - value)).argmin()
-    return array[idx]
+from tools.predict_contours import predict_contours
 
 
 def test_prediction(src_proj_dir):
-    solver = cfg.solver(cfg)
-    provider = cfg.provider_single
-
-    dest_img_fname = os.path.join(src_proj_dir,
-                                  'tmp_mppx{:.2f}.png'.format(cfg.mppx))
-    dest_himg_fname = os.path.join(src_proj_dir,
-                                   'htmp_mppx{:.2f}.png'.format(cfg.mppx)) if cfg.use_heightmap else None
-    is_success = PrepareData.build_from_project(src_proj_dir, cfg.mppx, dest_img_fname, dest_himg_fname)
-    if not is_success:
-        exit(-1)
-
-    dataset = provider(read_sample,
-                       dest_img_fname,
-                       dest_himg_fname,
-                       cfg,
-                       prep_getter=solver.get_prep_getter())
-
-    model = None
     # If enable following flag it will avoid long prediction and will try to read already created result.
     # Useful for debugging
     skip_prediction = False
-    if not skip_prediction:
-        model, weights_path, _ = solver.build(compile_model=False)
-        if model is None:
-            exit(-1)
 
+    err_code, result_dict = predict_contours(cfg, src_proj_dir, skip_prediction)
+    if err_code != 0:
+        return -1
+
+    pr_cntrs_list, dataset, solver = result_dict['contours_px'], result_dict['dataset'], result_dict['solver']
     image, _ = dataset[0]
     image_fname = dataset.get_fname(0)
-    predict_png = 'probability_' + os.path.splitext(os.path.basename(solver.weights_path))[0] + '.png'
-    # IMPORTANT:
-    # * Do not use size bigger than actual image size because blending(with generated border) will suppress actual
-    # prediction result.
-    window_size = int(find_nearest([64, 128, 256, 512, 1024], min(image.shape[0], image.shape[1])))
-    print('Window size in smoothing predicting: {}'.format(window_size))
-    if model is not None:
-        pr_mask = predict_img_with_smooth_windowing(
-            image,
-            window_size=window_size,
-            subdivisions=2,  # Minimal amount of overlap for windowing. Must be an even number.
-            nb_classes=cfg.cls_nb,
-            pred_func=(
-                lambda img_batch_subdiv: model.predict(img_batch_subdiv)
-            ),
-            use_batch_1=True
-        )
-        pr_mask = solver.post_predict(pr_mask)
-
-        cv2.imwrite(os.path.join(src_proj_dir, predict_png), (pr_mask * 255).astype(np.uint8))
-    else:
-        pr_mask = cv2.imread(os.path.join(src_proj_dir, predict_png), cv2.IMREAD_UNCHANGED).astype(np.float32) / 255.0
-        # Just to simulate shape of real generated data
-        if len(pr_mask.shape) == 2:
-            pr_mask = pr_mask[..., np.newaxis]
-
-    pr_cntrs_list = solver.get_contours(pr_mask)
 
     img_temp = (utilites.denormalize(image[..., :3]) * 255).astype(np.uint8)
     for class_ind, class_ctrs in enumerate(pr_cntrs_list):
@@ -90,10 +38,12 @@ def test_prediction(src_proj_dir):
     with open(os.path.join(src_proj_dir, output_filename), 'w', newline=os.linesep) as f:
         json.dump(via_item, f)
 
+    return 0
+
 
 if __name__ == "__main__":
 
-    proj_dir = 'F:/DATASET/Strayos/MuckPileDatasets.outputs/dyno/1334'  # Especially good for rocks
+    ## proj_dir = 'F:/DATASET/Strayos/MuckPileDatasets.outputs/dyno/1334'  # Especially good for rocks
     # proj_dir = 'F:/DATASET/Strayos/MuckPileDatasets.outputs/dyno/1341'  # small size
     # proj_dir = 'F:/DATASET/Strayos/MuckPileDatasets.unseen/airzaar/12105'  # unseen during training BIG
     # proj_dir = 'F:/DATASET/Strayos/MuckPileDatasets.unseen/airzaar/12120'  # unseen during training
@@ -104,11 +54,12 @@ if __name__ == "__main__":
     # proj_dir = 'F:/DATASET/Strayos/MuckPileDatasets.outputs/dev-site/3554'  # big size
     # proj_dir = 'F:/DATASET/Strayos/MuckPileDatasets.outputs/dev-site/3637'  # huge size(4GB-GPU impossible)
     # proj_dir = 'F:/DATASET/Strayos/MuckPileDatasets.unseen/airzaar/12976'  # BAD PRODUCTION RESULT. SMALL
-    ## proj_dir = 'F:/DATASET/Strayos/MuckPileDatasets.unseen/qa/7966'
+    proj_dir = 'F:/DATASET/Strayos/MuckPileDatasets.unseen/qa/7966'
     # proj_dir = 'F:/DATASET/Strayos/MuckPileDatasets.unseen/airzaar/12977'  # BAD PRODUCTION RESULT. SMALL
     # proj_dir = 'F:/DATASET/Strayos/MuckPileDatasets.unseen/qa/7965'
     # proj_dir = 'F:/DATASET/Strayos/MuckPileDatasets.unseen/airzaar/12989'  # BAD PRODUCTION RESULT
     # proj_dir = 'F:/DATASET/Strayos/MuckPileDatasets.unseen/qa/7964'
     # proj_dir = 'F:/DATASET/Strayos/MuckPileDatasets.unseen/airzaar/12189'
     # proj_dir = 'F:/DATASET/Strayos/MuckPileDatasets.unseen/qa/7969'
-    test_prediction(proj_dir)
+
+    exit(test_prediction(proj_dir))
---
+++ tools/predict_contours.py
@@ -0,0 +1,83 @@
+import os
+import sys
+import numpy as np
+import cv2
+sys.path.append(sys.path[0] + "/..")
+from kutils import PrepareData
+from kmodel.train import read_sample
+from kmodel.smooth_tiled_predictions import predict_img_with_smooth_windowing
+
+
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
+
+
+def predict_contours(cfg, src_proj_dir, skip_prediction=False, use_batch_1=True):
+    """
+    :param cfg:
+    :param src_proj_dir:
+    :param skip_prediction: If True following flag it will avoid long prediction and will try to read already
+    created result. Useful for debugging.
+    :param use_batch_1: If True, it will use less GPU
+    :return:
+    """
+    solver = cfg.solver(cfg)
+    provider = cfg.provider_single
+
+    dest_img_fname = os.path.join(src_proj_dir,
+                                  'tmp_mppx{:.2f}.png'.format(cfg.mppx))
+    dest_himg_fname = os.path.join(src_proj_dir,
+                                   'htmp_mppx{:.2f}.png'.format(cfg.mppx)) if cfg.use_heightmap else None
+
+    is_success = PrepareData.build_from_project(src_proj_dir, cfg.mppx, dest_img_fname, dest_himg_fname)
+    if not is_success:
+        print('ERROR: Cannot prepare data')
+        return -1, dict({})
+
+    dataset = provider(read_sample,
+                       dest_img_fname,
+                       dest_himg_fname,
+                       cfg,
+                       prep_getter=solver.get_prep_getter())
+
+    model = None
+    if not skip_prediction:
+        model, weights_path, _ = solver.build(compile_model=False)
+        if model is None:
+            print('ERROR: Cannot create model')
+            return -1, dict({})
+
+    image, _ = dataset[0]
+    predict_png = 'probability_' + os.path.splitext(os.path.basename(solver.weights_path))[0] + '.png'
+    # IMPORTANT:
+    # * Do not use size bigger than actual image size because blending(with generated border) will suppress actual
+    # prediction result.
+    window_size = int(find_nearest([64, 128, 256, 512, 1024], min(image.shape[0], image.shape[1])))
+    print('Window size in smoothing predicting: {}'.format(window_size))
+    if model is not None:
+        pr_mask = predict_img_with_smooth_windowing(
+            image,
+            window_size=window_size,
+            subdivisions=2,  # Minimal amount of overlap for windowing. Must be an even number.
+            nb_classes=cfg.cls_nb,
+            pred_func=(
+                lambda img_batch_subdiv: model.predict(img_batch_subdiv)
+            ),
+            use_batch_1=use_batch_1
+        )
+        pr_mask = solver.post_predict(pr_mask)
+
+        cv2.imwrite(os.path.join(src_proj_dir, predict_png), (pr_mask * 255).astype(np.uint8))
+    else:
+        fpath = os.path.join(src_proj_dir, predict_png)
+        print('Prediction skipped. Trying to read already predicted result from {}'.format(fpath))
+        pr_mask = cv2.imread(fpath, cv2.IMREAD_UNCHANGED).astype(np.float32) / 255.0
+        # Just to simulate shape of real generated data
+        if len(pr_mask.shape) == 2:
+            pr_mask = pr_mask[..., np.newaxis]
+
+    pr_cntrs_list_px = solver.get_contours(pr_mask)
+
+    return 0, dict({'contours_px': pr_cntrs_list_px, 'dataset': dataset, 'solver': solver})
